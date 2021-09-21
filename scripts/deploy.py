e3cdc9e42c737e2ab83dc33a1988e922024faa7c
dev-waleed <hiwaleed@outlook.com>
2021-09-21 21:56:14 +0500

    Using Brownie's Methods for Private Key Management

---
+++ scripts/deploy.py
@@ -0,0 +1,8 @@
+from brownie import accounts, config
+
+def deploy_simple_storage():
+    account = accounts.add(config["wallets"]["from_key"])
+    print(account)
+
+def main():
+    deploy_simple_storage()
